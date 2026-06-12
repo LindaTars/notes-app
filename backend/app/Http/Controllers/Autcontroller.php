3e54b6dbd1d87a68e9b994ceadb58d269cf2edfb
ED <laloangeles117@gmail.com>
2026-06-11 21:19:30 -0600

    Ya es funcional el login

--- backend/app/Http/Controllers/Autcontroller.php
+++ backend/app/Http/Controllers/Autcontroller.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 // class Autcontroller extends Controller
 class Autcontroller extends Controller
 {
-
+    //! Funcion Crear Cuenta
     //* Registra un cuenta de usuario 
     public function registrar(Request $request){
         //* Validacion de datos de desde afuera(react )
@@ -16,9 +16,8 @@ class Autcontroller extends Controller
             'name' => ['required', 'string', 'max:225'],
             'lastName' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:100', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'] 
-
-            
+            'password' => ['required', 'string', 'min:8'],
+            'userName'=>['required', 'string', 'max:100', 'unique:users'],                
         ]);
         //* Los datos de lo anterior se mandaran al modelo para  crear el usuario en postgres
         //*Inyeccion de dependencias
@@ -26,7 +25,8 @@ class Autcontroller extends Controller
             'name' => $request->name,
             'lastName' =>$request->lastName,
             'email'=> $request->email,
-            'password'=> Hash::make($request->password)//*Se encripta la contraseña 
+            'password'=> Hash::make($request->password),//*Se encripta la contraseña 
+            'userName'=> $request->userName
         ]);
         //* Maneja una respuesta si se creo el usuario correctamente
         return response()->json([
@@ -34,8 +34,34 @@ class Autcontroller extends Controller
             'message' => 'ya se creo la cuenta',
             'data' => $user
          ], 201);
-
     }
+    //!Funcion Iniciar secion 
+    public function ingresar(Request $request){
+        $request -> validate([
+            'userName' => ['required', 'string', 'max:100'],
+            'password' => ['required', 'string', 'min:8']   
+        ]);
+
+        //* busca la existencia de algun usuario con ese nombre de usuario
+        $user = User::where('userName', $request->userName)->first();
+        //* Valida las contraseñas y que el usuairo exista 
+          if (!$user || !Hash::check($request ->password, $user ->password)){
+            return response()->json([
+                'status'=>'error',
+                'message' => 'contraseña incorrecta',
+                'data' => $user
+            ], 401);
+         }else{
+            return response()->json([
+                'status'=>'success',
+                'message' => 'ingreso correctamente ',
+                'data' => $user
+            ], 201);   
+  
+        };
+       
+    }
+
 
 }
 //Todo: Lo siguiente es hacer las rutas para manegar las acciones jsjsj
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 // * Aqui se pondran las rutas para registrar un nuevo usuario
 Route::post('/registro', [Autcontroller::class,'registrar']);
+//* Ruta pa hacer el logeo
+Route::post('/login', [Autcontroller::class,'ingresar']);
